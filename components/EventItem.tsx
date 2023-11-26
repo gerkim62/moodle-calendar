6d280bdca1f426493b27810503be17e4aaef86b6
Gerison Kimathi Muriungi <72649107+gerkim62@users.noreply.github.com>
2023-11-26 18:38:03 +0000

    Fix overdue event display

--- components/EventItem.tsx
+++ components/EventItem.tsx
@@ -64,7 +64,14 @@ const EventItem: React.FC<EventProps> = ({ event }) => {
 
   return (
     <div className="bg-white dark:bg-slate-900 shadow p-6 rounded-md border border-gray-200 dark:border-gray-700 relative">
-      <h2 className="text-xl font-semibold mb-4">{event.summary}</h2>
+      <h2 className="text-xl font-semibold mb-4">{event.summary}    {
+        new Date(event.end || "").getTime() < new Date().getTime() &&<span className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+        <svg className="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z"/>
+        </svg>
+        Overdue
+        </span>
+      }</h2>
       {/* {event.uid && (
         <form
           action={(data) => {
@@ -79,6 +86,8 @@ const EventItem: React.FC<EventProps> = ({ event }) => {
       )} */}
       <p className="text-gray-600 dark:text-gray-400 mb-4">
         <strong>Due Date:</strong> {getDueDate()} ({formaDayAndTime(new Date(event.start || ""))})
+      {/* overdue */}
+   
       </p>
       <p className="text-gray-600 dark:text-gray-400 mb-4">
         <strong>Last Modified:</strong>{" "}
