206baca5555dc45ead533d37b0ca972e30fdd2f2
Gerison Kimathi Muriungi <72649107+gerkim62@users.noreply.github.com>
2023-11-26 19:17:10 +0000

    Update loading component styles

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="border-t-4 border-blue-500 rounded-full animate-spin h-14 w-14 mr-3"></div>
+    <div className="flex items-center justify-center my-5">
+      <div className="border-t-4 border-blue-500 rounded-full animate-spin h-14 w-14 mr-3 "></div>
       <span className="text-xl font-semibold text-blue-500 dark:text-blue-300">
         Loading...
       </span>
