b4a874fff39c2948665a2cb377e883865993ce9e
Gerison Kimathi Muriungi <72649107+gerkim62@users.noreply.github.com>
2023-11-26 13:12:40 +0000

    added about component and contact

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="bg-gray-100 dark:bg-gray-800 max-w-md w-[90vw] m-4 mx-auto">
+            <div className="container mx-auto py-8">
+                <h1 className="text-3xl font-bold mb-4">About Me</h1>
+                <p className="text-lg">
+                    I am a software engineering student at the University of Eastern
+                    Africa. I have a passion for web development and enjoy working with
+                    React, Next.js, and Tailwind CSS.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
