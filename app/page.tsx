db01871a352fb3cc2731d6e952b95d33ad5c0084
Gerison Kimathi Muriungi <72649107+gerkim62@users.noreply.github.com>
2023-11-27 07:43:11 +0000

    Add loading state to error button

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,16 @@ import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa";
 import { getServerSession } from "next-auth";
 
-
-
-
-
 export const metadata = {
   title: "Calendify | Home",
-  description: "View your Moodle elearning calendar events without having to log in to Moodle.",
-  keywords: "moodle, elearning, dashboard, calendar, events, ueab, university of eastern africa baraton",
+  description:
+    "View your Moodle elearning calendar events without having to log in to Moodle.",
+  keywords:
+    "moodle, elearning, dashboard, calendar, events, ueab, university of eastern africa baraton",
 };
 
-const Homepage: React.FC =async () => {
-  const session =await getServerSession();
+const Homepage: React.FC = async () => {
+  const session = await getServerSession();
   return (
     <div className="bg-gray-100 dark:bg-gray-900 min-h-screen flex justify-center mb-4">
       <div className="max-w-lg mt-6 mx-4">
@@ -38,7 +36,8 @@ const Homepage: React.FC =async () => {
           portal visits by notifying you of pending tasks, ensuring you never
           miss a deadline.
         </p>
-        {(session && session.user)? <div className="flex flex-col sm:flex-row sm:justify-between items-center">
+        {session && session.user ? (
+          <div className="flex flex-col sm:flex-row sm:justify-between items-center">
             <Link
               href="/dashboard"
               className="w-full sm:w-auto mt-2 sm:mt-0 mb-2 sm:mb-0 relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
@@ -47,30 +46,34 @@ const Homepage: React.FC =async () => {
                 View my Events <FaArrowRight className="ml-2" />
               </span>
             </Link>
-          </div>:<><div className="flex space-x-4">
-          <div className="flex flex-col sm:flex-row sm:justify-between items-center">
-            <Link
-              href="/signup"
-              className="w-full sm:w-auto mt-2 sm:mt-0 mb-2 sm:mb-0 relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-            >
-              <span className="relative flex justify-center items-center gap-2 px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-full">
-                Get Started Now <FaArrowRight className="ml-2" />
-              </span>
-            </Link>
           </div>
-     
-        </div>
-        <p className="mt-4 dark:text-white text-gray-600">
-          Already have an account?{" "}
+        ) : (
           <>
-            <Link
-              href="/signin"
-              className="text-purple-500 dark:text-purple-300 hover:underline"
-            >
-              Login here
-            </Link>
+            <div className="flex space-x-4">
+              <div className="flex flex-col sm:flex-row sm:justify-between items-center">
+                <Link
+                  href="/signup"
+                  className="w-full sm:w-auto mt-2 sm:mt-0 mb-2 sm:mb-0 relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+                >
+                  <span className="relative flex justify-center items-center gap-2 px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-full">
+                    Get Started Now <FaArrowRight className="ml-2" />
+                  </span>
+                </Link>
+              </div>
+            </div>
+            <p className="mt-4 dark:text-white text-gray-600">
+              Already have an account?{" "}
+              <>
+                <Link
+                  href="/signin"
+                  className="text-purple-500 dark:text-purple-300 hover:underline"
+                >
+                  Login here
+                </Link>
+              </>
+            </p>
           </>
-        </p></>}
+        )}
       </div>
     </div>
   );
