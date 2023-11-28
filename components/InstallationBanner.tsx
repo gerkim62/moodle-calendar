354b45c88ff4636c7d0d1f2b62f4f4617cda4da1
gerkim62 <gerkim62@gmail.com>
2023-11-28 13:33:57 +0300

    changed icon

--- components/InstallationBanner.tsx
+++ components/InstallationBanner.tsx
@@ -77,19 +77,11 @@ const InstallationBanner: React.FC = () => {
             >
               Install Now{" "}
               <svg
-                className="w-3 h-3 ms-2 rtl:rotate-180"
-                aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
+                height="0.625em"
+                viewBox="0 0 448 512"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
+                <path d="M349.4 44.6c5.9-13.7 1.5-29.7-10.6-38.5s-28.6-8-39.9 1.8l-256 224c-10 8.8-13.6 22.9-8.9 35.3S50.7 288 64 288H175.5L98.6 467.4c-5.9 13.7-1.5 29.7 10.6 38.5s28.6 8 39.9-1.8l256-224c10-8.8 13.6-22.9 8.9-35.3s-16.6-20.7-30-20.7H272.5L349.4 44.6z" />
               </svg>
             </button>
           </span>
